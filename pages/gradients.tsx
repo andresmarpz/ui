252f2bbcaad1804facce654f03b7b4616399f9a7
andresmarpz <andresmarpz@gmail.com>
2022-09-13 15:00:43 -0300

    feat: add head to gradients page

--- pages/gradients.tsx
+++ pages/gradients.tsx
@@ -1,12 +1,23 @@
-import TextGradient from "@/components/gradients/TextGradient";
-import { NextPage } from "next";
+import Title from '@/components/global/Title'
+import TextGradient from '@/components/gradients/TextGradient'
+import { NextPage } from 'next'
+import Head from 'next/head'
 
 const Gradients: NextPage = () => {
-	return <>
-		<h1>Gradients!</h1>
+    return (
+        <>
+			<Head>
+				<title>Gradients</title>
+			</Head>
+            <Title size="large" css={{ marginTop: 40 }}>
+                Gradients
+            </Title>
 
-		<TextGradient gradient="linear-gradient(to right, #30CFD0 0%, #330867 100%)">this is a text gradient</TextGradient>
-	</>
+            <TextGradient gradient="linear-gradient(to right, #30CFD0 0%, #330867 100%)">
+                this is a text gradient
+            </TextGradient>
+        </>
+    )
 }
 
 export default Gradients
